7fd86ac653d5ccf0c497887abb35d4def097e221
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-04-17 15:35:39 +0200

    add initial grid layout

---
+++ RestaurantManager/Controls/GridButton.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+
+namespace RestaurantManager.Controls
+{
+    public class GridButton : Button
+    {
+        public GridButton() : base()
+        {
+            BackgroundColor = Color.AliceBlue;
+            CornerRadius = 10;
+            Margin = 15;
+        }
+    }
+}
