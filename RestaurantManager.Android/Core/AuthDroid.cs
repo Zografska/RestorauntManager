c3c986268ad35d3f564aaa663f6b1a71a044c8e4
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-07-16 11:06:35 +0200

    Add Auth for android (needs to be checked on real device)

---
+++ RestaurantManager.Android/Core/AuthDroid.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Android.Gms.Extensions;
+using Firebase.Auth;
+using RestaurantManager.Core.Authentication;
+using RestaurantManager.Droid.Core;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AuthDroid))]
+namespace RestaurantManager.Droid.Core
+{
+    public class AuthDroid : IAuthService
+    {
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            try 
+            {
+                var user = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdToken(false);
+                return token.ToString();
+            } 
+            catch(FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return "";
+            }
+        }
+
+        public async Task<bool> SignUpWithEmailPassword(string email, string password)
+        {
+            try
+            {
+                var signUpTask = FirebaseAuth.Instance.CreateUserWithEmailAndPassword(email, password);
+
+                return signUpTask.Result != null;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
