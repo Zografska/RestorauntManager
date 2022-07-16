22a830f05442884cb179a300644fd2cdbe26ac02
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-07-16 22:33:12 +0200

    add validation to login page

--- RestaurantManager/Pages/Authentication/Login/LoginPageViewModel.cs
+++ RestaurantManager/Pages/Authentication/Login/LoginPageViewModel.cs
@@ -14,16 +14,35 @@ namespace RestaurantManager.Pages.Authentication.Login
         private readonly IAuthService _authService;
         private string _username { get; set; }
         private string _password { get; set; }
+        private bool _usernameValid { get; set; }
+        private bool _passwordValid { get; set; }
+
+        public bool IsLoginPossible => _usernameValid && _passwordValid;
         public ICommand LoginCommand { get; set; }
         public ICommand NavigateToSignupCommand { get; set; }
-        
+        public bool PasswordValid
+        {
+            set
+            {
+                _passwordValid = value && !Username.IsNullOrEmpty();
+                RaisePropertyChanged(nameof(IsLoginPossible));
+            }
+        } 
+        public bool UsernameValid
+        {
+            set
+            {
+                _usernameValid = value && !Password.IsNullOrEmpty();
+                RaisePropertyChanged(nameof(IsLoginPossible));
+            }
+        } 
         public string Username
         {
             get => _username;
             set
             {
                 _username = value;
-                RaisePropertyChanged(Username);
+                RaisePropertyChanged(nameof(Username));
             }
         } 
         public string Password
@@ -32,7 +51,7 @@ namespace RestaurantManager.Pages.Authentication.Login
             set
             {
                 _password = value;
-                RaisePropertyChanged(Password);
+                RaisePropertyChanged(nameof(Password));
             }
         }
         
@@ -50,12 +69,6 @@ namespace RestaurantManager.Pages.Authentication.Login
 
         private async void Login()
         {
-            if (Username.IsNullOrEmpty() || Password.IsNullOrEmpty())
-            {
-                DisplayAlert(Constants.AlertConstants.ValidationAlert);
-                return;
-            }
-            
             var token = await _authService.LoginWithEmailPassword(Username, Password);
             if (!token.IsNullOrEmpty())
             {
