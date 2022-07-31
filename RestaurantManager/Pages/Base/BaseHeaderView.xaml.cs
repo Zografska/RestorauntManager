3494bd3a265842a87bb20be39bbf9feed7c8d2e7
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-07-31 13:01:15 +0200

    refactor the header in a seperate view

---
+++ RestaurantManager/Pages/Base/BaseHeaderView.xaml.cs
@@ -0,0 +1,13 @@
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantManager.Pages.Base
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BaseHeaderView
+    {
+        public BaseHeaderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
