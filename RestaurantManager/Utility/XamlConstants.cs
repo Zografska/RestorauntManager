1add791d45eade96347b6ff99ae2e8b5b6d51977
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-07-31 18:18:35 +0200

    add constants

--- RestaurantManager/Utility/XamlConstants.cs
+++ RestaurantManager/Utility/XamlConstants.cs
@@ -4,5 +4,12 @@ namespace RestaurantManager.Utility
     {
         public const string SharedNotes = "SharedNotes";
         public const string MyNotes = "MyNotes";
+        public const string Delete = "Delete";
+        public const string Save = "Save";
+        public const string Title = "Title";
+        public const string Description = "Description";
+        public const string LastModified = "Last Modified";
+        public const string Creator = "Creator";
+        public const string AddNote = "Add Note";
     }
 }
