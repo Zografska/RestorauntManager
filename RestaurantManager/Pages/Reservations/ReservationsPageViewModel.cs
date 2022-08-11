1300fe6a1bb98baec5367ed419d657466568d2e6
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-08-11 22:14:35 +0200

    initial calendar view

--- RestaurantManager/Pages/Reservations/ReservationsPageViewModel.cs
+++ RestaurantManager/Pages/Reservations/ReservationsPageViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.ObjectModel;
 using Prism.Navigation;
 using RestaurantManager.Core.Authentication;
 using RestaurantManager.Pages.Base;
@@ -12,6 +13,26 @@ namespace RestaurantManager.Pages.Reservations
 {
     public class ReservationsPageViewModel : PageViewModelBase
     {
+        private ObservableCollection<string> _items = new ObservableCollection<string>()
+        {
+            "1","2","3","4","5","6","7"
+        };
+        public ObservableCollection<string> Week1
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+        
+        private ObservableCollection<string> _daysOfWeek = new ObservableCollection<string>()
+        {
+        
+                "M","T","W","T","F","S","S"
+        };
+        public ObservableCollection<string> DaysOfWeek
+        {
+            get => _daysOfWeek;
+            set => SetProperty(ref _daysOfWeek, value);
+        }
         public Command DateTappedCommand { get; }
         public ReservationsPageViewModel(INavigationService navigationService, IPopupService popupService,
             IAuthService authService, INetworkService networkService) 
