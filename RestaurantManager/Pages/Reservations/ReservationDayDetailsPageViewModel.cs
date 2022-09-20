ae709f6439ab7632e6e66a53fd6890265d401758
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-09-20 22:09:05 +0200

    implement reservation notifications

--- RestaurantManager/Pages/Reservations/ReservationDayDetailsPageViewModel.cs
+++ RestaurantManager/Pages/Reservations/ReservationDayDetailsPageViewModel.cs
@@ -17,27 +17,31 @@ namespace RestaurantManager.Pages.Reservations
     public class ReservationDayDetailsPageViewModel : ListViewModel<Reservation>
     {
         private readonly IReservationService _reservationService;
+        private readonly IPushNotificationsLocal _pushNotificationsLocal;
+
         private DateTime Day { get; set; }
-        
-        protected ReservationDayDetailsPageViewModel(INavigationService navigationService, IPopupService popupService,
+
+        protected ReservationDayDetailsPageViewModel(IPushNotificationsLocal pushNotificationsLocal,
+            INavigationService navigationService, IPopupService popupService,
             DatabaseServiceRemote databaseServiceRemote, INetworkService networkService,
-            IReservationService reservationService) 
+            IReservationService reservationService)
             : base(navigationService, popupService, databaseServiceRemote, networkService)
         {
+            _pushNotificationsLocal = pushNotificationsLocal;
             _reservationService = reservationService;
             _service = reservationService;
             Title = "Reservations";
             PopupName = nameof(ReservationCNIPopup);
         }
-        
+
         public override async void Initialize(INavigationParameters parameters)
         {
             base.Initialize(parameters);
             if (NetworkService.IsNetworkConnected())
-            {            
+            {
                 parameters.TryGetValue<DateTime>(Constants.NavigationConstants.Date, out var date);
                 Day = date;
-                
+
                 if (!date.IsDefaultDate())
                 {
                     Items = await _reservationService.GetAllByDate(date);
@@ -63,6 +67,11 @@ namespace RestaurantManager.Pages.Reservations
                     Items.Remove(newReservation);
                 }
             }
+
+            if (newReservation != null)
+                _pushNotificationsLocal.SendNotification("Reservation soon!",
+                    $"Reservation for {newReservation.NumberOfPeople} comming up in 1 minute. Name: {newReservation.OnName}",
+                    newReservation.ReservationDate.AddSeconds(60));
 
             return newReservation;
         }
